90a0ba81eadfb8b76a603cadc70a04bd3459cb81
1xAbel <111407812+1xAbel@users.noreply.github.com>
2022-11-14 20:24:45 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,7 @@ local games = {
     [{10476933477}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/strongninjasim.lua",
     [{9498006165}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/tapping-simulator.lua",
     [{8540346411}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/rebirth-champions.lua",
+    [{4723618670}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/Wisteria-Revamped.lua",
 }
 
 for ids, url in next, games do
