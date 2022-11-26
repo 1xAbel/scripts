f8f74163d172b8e0a73ede8b53ba2afdb4518104
1xAbel <111407812+1xAbel@users.noreply.github.com>
2022-11-25 21:52:08 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,7 @@ local games = {
     [{8540346411}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/rebirth-champions.lua",
     [{4723618670}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/Wisteria-Revamped.lua",
     [{7525610732}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/Kaizen.lua",
+    [{1499872953}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/boku-no-roblox.lua",
     [{11468159863, 6152116144}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ProjectSlayers.lua",
 }
 
