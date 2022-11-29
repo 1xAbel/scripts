22c8192a8c7a92d2e5e4e3c921c4aab7679b65d0
1xAbel <111407812+1xAbel@users.noreply.github.com>
2022-11-28 23:34:05 -0500

    Delete projectslayers.lua

--- projectslayers.lua
+++
@@ -1,9 +0,0 @@
-getgenv().killaura = true
-while killaura do wait()
-  local args = {
-    [1] = "blacks make me mad",
-    [2] = "All"
-  }
-
-  game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-end
