590a05b662b239ab06499ce2b13edf5eaa1cc88b
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-09-11 18:14:14 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,7 @@ local games = {
     [{3823781113}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/SaberSimulator.lua",
     [{11468159863, 6152116144}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ProjectSlayers.lua",
     [{11040063484}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/SwordFightersSimulator.lua",
+    [{13772394625}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/blade-ball.lua",
 }
 
 for ids, url in next, games do
