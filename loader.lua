e47ace02a54972c72094b829c70ce041ef4feb60
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-09-11 17:27:00 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,8 @@
 local games = {
-    [{4723618670}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/Wisteria-Revamped.lua",
     [{3956818381}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ninja-legends.lua",
     [{3823781113}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/SaberSimulator.lua",
     [{11468159863, 6152116144}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ProjectSlayers.lua",
     [{11040063484}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/SwordFightersSimulator.lua",
-    [{11884594868, 11885022882}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/AnimeDefenseSimulator.lua",
-    [{8131063774}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ShatteredPsychoOnline.lua",
-    [{1240123653}] = "https://raw.githubusercontent.com/1xAbel/scripts/main/ZombieAttack.lua",
 }
 
 for ids, url in next, games do
